2d257099eaca8ba88cf4e3aa65cffc76d84447f0
QuadM <mahmoudhafez1511@gmail.com>
2022-05-03 12:43:51 +0200

    added stories page

---
+++ js/stories-page.js
@@ -0,0 +1,96 @@
+const stories_page = document.querySelector("#stories-page");
+const main_content = document.querySelector("#maincontent");
+const home = document.querySelector("#home");
+
+const get_data = async (option) => {
+  const data = fetch(`http://localhost:3000/draft/${option}`, {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    method: "GET",
+    body: JSON.stringify(),
+  })
+    .then((response) => response.json())
+    .then((r) => console.log(r))
+    .catch((e) => console.error(e));
+};
+
+const createDraft = (id, title, date) => {
+  const timeinyears = (Date.now() - date) / (1000 * 60 * 60 * 24 * 365);
+  const timeinMonths = timeinyears * 12;
+  const timeinDays = timeinMonths * 30;
+  const timeinWeeks = timeinDays / 7;
+  const timeinHrs = timeinDays * 24;
+  const timeinMins = timeinHrs * 60;
+
+  const timetxt =
+    timeinyears > 1
+      ? timeinyears.toFixed(0) + " years "
+      : timeinMonths > 1
+      ? timeinMonths.toFixed(0) + " months "
+      : timeinWeeks > 1
+      ? timeinWeeks.toFixed(0) + " weeks "
+      : timeinDays > 1
+      ? timeinDays.toFixed(0) + " days "
+      : timeinHrs > 1
+      ? timeinHrs.toFixed(0) + " hrs "
+      : timeinMins.toFixed(0) + " min ";
+
+  const draft = document.createElement("div");
+  draft.innerHTML =
+    //   <a href="/draft/${id}"><b style="color:black;font-size:16px">${title}</b></a>
+    `
+  <a href="#"><b style="color:black;font-size:16px;">${title}</b></a>
+  <p style='opacity:50%;'> Created ${timetxt} ago </p>   
+  <hr>
+  `;
+  draft.style.font = "14px Arial, sans-serif";
+  draft.style.marginTop = "20px";
+  return draft;
+};
+const drafts = [
+  {
+    id: "34rtyui8vds7",
+    title: "Untitled story 1",
+    time: Date.UTC(2021),
+  },
+  {
+    id: "34rtdsayui87",
+    title: "Untitled story 2",
+    time: Date.UTC(2022, 3),
+  },
+  {
+    id: "34rtyuidsa87",
+    title: "Untitled story 3",
+    time: Date.UTC(2022, 4, 1),
+  },
+  {
+    id: "34rtyui8fas7",
+    title: "Untitled story 4",
+    time: Date.now(),
+  },
+  {
+    id: "34rtyui8ewf7",
+    title: "Untitled story 5",
+    time: Date.UTC(2020, 6),
+  },
+];
+
+const options = document.querySelectorAll("#options li");
+
+options.forEach((option) => {
+  option.addEventListener("click", (e) => {
+    options.forEach((o) => o.classList.remove("active"));
+    while (main_content.firstChild)
+      main_content.removeChild(main_content.firstChild);
+    option.classList.add("active");
+    // const data = await get_data(option.id)
+    const data = drafts;
+    option.id === "drafts"
+      ? drafts.forEach(({ id, title, time }) => {
+          main_content.appendChild(createDraft(id, title, time));
+        })
+      : null;
+  });
+});
