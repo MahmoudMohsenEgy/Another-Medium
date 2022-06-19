9993e232a64f441c9711fde9b88ac229359071ad
Yousef <yousefshafik234@yahoo.com>
2022-06-19 02:02:53 +0200

    added signin and signup pages

---
+++ js/signup-in.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded',()=>{
+    const inputs = document.querySelectorAll('input')
+    inputs.forEach((input)=>{
+        input.addEventListener("focusin",(e)=>{
+            const label = input.previousElementSibling.previousElementSibling
+            label.classList.remove('translate_down')
+            label.classList.add('translate_up')
+        })
+
+        input.addEventListener("blur",()=>{
+            const label = input.previousElementSibling.previousElementSibling
+            console.log("here")
+            label.classList.remove('translate_up')
+            label.classList.add('translate_down')
+        })
+    })
+
+
+})
